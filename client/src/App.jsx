298dccde877e123a582547393630e046b4132abe
linganago <linganagoudamaligouda@gmail.com>
2025-06-11 21:50:49 +0530

    Routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Profile from './pages/Profile'
+import About from './pages/About'
 const App = () => {
   return (
-    <div>
-      hii
-    </div>
+  <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/about' element={<About/>}/>
+    <Route path='/sign-in' element={<Signin/>}/>
+    <Route path='/sign-up' element={<Signup/>}/>
+    <Route path='/profile' element={<Profile/>}/>
+   </Routes>
+  </BrowserRouter>
   )
 }
 
