7108ed739286024f7aa1fb54a6747555e24ea911
linganago <linganagoudamaligouda@gmail.com>
2025-06-11 23:28:38 +0530

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,20 @@
-import { log } from 'console';
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config(); // ❗ You forgot to call the function
+
+// ❗ Correct the parentheses around `.then().catch()`
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
 
 const app = express();
 
-app.listen(3000,()=>{
-    console.log('server is running');
-    
-})
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
