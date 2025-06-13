0c996f5c9acc45e1cc9c3fa0178c26bc1d6c21cf
linganago <linganagoudamaligouda@gmail.com>
2025-06-13 10:24:24 +0530

    create a middleware that handle error

--- api/index.js
+++ api/index.js
@@ -24,3 +24,13 @@ app.use('/api/auth', authRouter); // 👈 register auth route
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+app.use((err,req,res,next)=>{
+  const statusCode= err.statusCode || 500;
+  const message = err.message || 'Internal server error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  })
+})
