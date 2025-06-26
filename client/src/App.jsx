8378efe4b1157d2390cf11a2f025cce3c05c755e
linganago <linganagoudamaligouda@gmail.com>
2025-06-26 11:42:14 +0530

    create and update listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import About from './pages/About'
 import Header from './Components/Header.jsx'
 import PrivateRoute from './Components/privateRoute.jsx'
 import CreateListing from './pages/CreateListing.jsx'
+import UpdateListing from './pages/UpdateListing.jsx'
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
     <Route element={<PrivateRoute/>}>
     <Route path='/profile' element={<Profile/>}/>
     <Route path='/create-listing' element={<CreateListing/>}/>
+    <Route path='/update-listing/:listingId' element={<UpdateListing/>}/>
     </Route>
    </Routes>
   </BrowserRouter>
